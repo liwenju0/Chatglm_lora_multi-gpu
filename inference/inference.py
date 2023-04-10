1b2ac27c2f1a1929e4fb3861cd4e92aa427d4b52
liwenju <wenju.li@deepctrl.cn>
2023-04-10 18:38:08 +0800

    完成部署

--- inference/inference.py
+++ inference/inference.py
@@ -1,17 +1,22 @@
+import sys
+sys.path.append("/home/deepctrl/Chatglm_lora_multi-gpu/")
 from modeling_chatglm import ChatGLMForConditionalGeneration
+from tokenization_chatglm import ChatGLMTokenizer
 import torch
 from transformers import AutoTokenizer
 from peft import get_peft_model, LoraConfig, TaskType
 
 #设置环境，CPU还是GPU
 torch.set_default_tensor_type(torch.cuda.HalfTensor)
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 print("Device:", device)
+# model_path = "THUDM/chatglm-6b"
+model_path = "/home/deepctrl/Chatglm_lora_multi-gpu/model_weight"
 #加载base model
-model = ChatGLMForConditionalGeneration.from_pretrained("THUDM/chatglm-6b", cache_dir='./',trust_remote_code=True).half().to(device)
+model = ChatGLMForConditionalGeneration.from_pretrained(model_path,trust_remote_code=True).half().to(device)
 
 #设置tokenizer
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b",cache_dir ='./', trust_remote_code=True)
+tokenizer = ChatGLMTokenizer.from_pretrained(model_path, trust_remote_code=True)
 
 #加载训练好的lora
 peft_path = "output/chatglm-lora.pt"
@@ -22,13 +27,14 @@ peft_config = LoraConfig(
     lora_alpha=32, lora_dropout=0.1
 )
 
-model = get_peft_model(model, peft_config)
-model.load_state_dict(torch.load(peft_path), strict=False)
+# model = get_peft_model(model, peft_config)
+# model.load_state_dict(torch.load(peft_path), strict=False)
 torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
 #模型生成
-text = "你是现代诗人，用'红包、美好、表白、夕阳、月光、慢慢'关键词生成2首表白唯美打油诗\n"
+text = "你是现代诗人，用'红包、美好、表白、夕阳、月光、慢慢'关键词生成2首表白唯美打油诗"
 input_ids = tokenizer.encode(text, return_tensors='pt')
+input_ids = input_ids.to(device)
 #model.cuda()
 out = model.generate(input_ids=input_ids,max_length=2048,temperature=0.4)
 answer = tokenizer.decode(out[0])
